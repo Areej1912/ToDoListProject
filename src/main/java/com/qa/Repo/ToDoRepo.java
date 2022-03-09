b58d3982f4b4f097cb5a5994e6520d0dc02edc85
Areej1912 <97736441+Areej1912@users.noreply.github.com>
2022-03-09 17:30:27 +0000

    Created repo interface with custom finder methods

--- src/main/java/com/qa/Repo/ToDoRepo.java
+++ src/main/java/com/qa/Repo/ToDoRepo.java
@@ -1,9 +1,14 @@
 package com.qa.Repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.qa.entity.ToDoItem;
 
 public interface ToDoRepo extends JpaRepository<ToDoItem, Long> {
 
+	List<ToDoItem> findByName(String name);
+	List<ToDoItem>findByPublished(boolean published);
+	
 }
