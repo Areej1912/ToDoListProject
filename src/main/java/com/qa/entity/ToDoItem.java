7c51a6fc746a908095c5648feeb770f78ae2afaf
Areej1912 <97736441+Areej1912@users.noreply.github.com>
2022-03-10 14:53:46 +0000

    added create functionality to service

--- src/main/java/com/qa/entity/ToDoItem.java
+++ src/main/java/com/qa/entity/ToDoItem.java
@@ -1,5 +1,6 @@
 package com.qa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,7 +14,9 @@ public class ToDoItem {
 	@Id 
 	@GeneratedValue(strategy = GenerationType.AUTO) 
 	private Long id;
+	@Column 
 	private String name; 
+	@Column
 	private boolean complete;
 
 	public ToDoItem() {}
--- src/main/java/com/qa/service/ToDoService.java
+++ src/main/java/com/qa/service/ToDoService.java
@@ -1,5 +1,26 @@
 package com.qa.service;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.Repo.ToDoRepo;
+import com.qa.entity.ToDoItem;
+
+@Service
 public class ToDoService {
 
+
+	private ToDoRepo repo;
+
+	public ToDoItem createToDoItem(ToDoItem todoitem) {
+	
+		return this.repo.save(todoitem); 
+	}
+
+
+	public boolean deleteToDoItem(Long id) {
+		// TODO Auto-generated method stub
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
+	
 }
