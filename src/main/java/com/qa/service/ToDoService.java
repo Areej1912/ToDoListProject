4bfc5c5746ee4313ba0e9be768192c84c4646659
Areej1912 <97736441+Areej1912@users.noreply.github.com>
2022-03-10 16:20:40 +0000

    added update functionality to service

--- src/main/java/com/qa/service/ToDoService.java
+++ src/main/java/com/qa/service/ToDoService.java
@@ -1,5 +1,9 @@
 package com.qa.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.qa.Repo.ToDoRepo;
@@ -24,12 +28,29 @@ public class ToDoService {
 	}
 
 
-	public ToDoItem updateToDoItem(Long id, ToDoItem todoitem) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public List<ToDoItem> getALLToDoItems() {
+		return this.repo.findAll();
 	}
-	
-	
-	
+
+
+	public ToDoItem getToDoItemById(Long id) {
+		Optional<ToDoItem> todoitemget = this.repo.findById(id);
+		if (todoitemget.isPresent()) {
+			return todoitemget.get();	
+		} else {
+			throw new EntityNotFoundException("To do item not found using this Id!");
+		}
+		
+				
+	}
+
+	public ToDoItem updateToDoItem(Long id, ToDoItem todoitem) {
+		ToDoItem foundToDoItem = this.getToDoItemById(id);
+		foundToDoItem.setName(todoitem.getName());
+		foundToDoItem.setComplete(todoitem.isComplete());
+		
+		return this.repo.save(foundToDoItem);
+	}
 	
 }
